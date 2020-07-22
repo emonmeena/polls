a651041b8fc355c5eaa33ee703542c079d566659
mayank-genesis <mayank16meena@gmail.com>
2020-07-22 20:09:36 +0530

    lecture 2 begins

--- mainwebsite/polls/views.py
+++ mainwebsite/polls/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
+
 def index(request):
     return HttpResponse("Django: inside polls app.")
+
+def show_candidates(request):
+    return HttpResponse("polls: showing candidates")
+
