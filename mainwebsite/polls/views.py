3a943bca06e20c499ea3e59b11dc195cca8e7bc5
mayank-genesis <mayank16meena@gmail.com>
2020-07-24 18:24:15 +0530

    done upto part 3

--- mainwebsite/polls/views.py
+++ mainwebsite/polls/views.py
@@ -1,9 +1,19 @@
-from django.http import HttpResponse
-
+from django.http import HttpResponse, Http404
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 def index(request):
-    return HttpResponse("Django: inside polls app.")
+    latest_question_list = Question.objects.order_by('-pub_date')
+    context = { 'latest_questions_list': latest_question_list } 
+    return render(request, 'polls/index.html', context)
 
-def show_candidates(request):
-    return HttpResponse("polls: showing candidates")
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk = question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
